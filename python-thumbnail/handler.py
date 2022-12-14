2f2cdb4b4f576b04f6c349a0298c3d056d83aed0
delyanmarinov <dmarinov@yotpo.com>
2022-12-14 14:06:46 +0200

    progress

--- python-thumbnail/handler.py
+++ python-thumbnail/handler.py
@@ -98,6 +98,48 @@ def s3_get_item(event, context):
 
     return {
         'statusCode':200,
-        'headers'
+        'headers': {
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin' : '*'
+        },
+        'body': json.dumps(item),
+        'isBase64Encoded' : False
     }
+
+def s3_delete_item(event, context):
+    item_id = event['pathParameters']['id']
+
+    response = {
+        'statusCode' : 500,
+        'body' : f'An error occured while deleting post {item_id}'
+    }
+    table = dynamodb.Table(dbtable)
+    response = table.delete_item(
+        Key={
+            'id':item_id
+        }
+    )
+
+    ok_response = {
+        'deleted': True,
+        'itemDeletedId': item_id
+    }
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json','Access-Control-Allow-Origin':'*'},
+            'body': json.dump(ok_response)
+        }
+
+    return response
+
+def s3_get_thumbnail_urls(event, context):
+    table = dynamodb.Table(dbtable)
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEveluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEveluatedKey'])
+        
 
